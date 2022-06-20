79521750d88fb4b03ca74d3eb53116ff62ed872a
Muhammad Ahmad Zubairu <muhdzubairuahmad@gmail.com>
2022-06-20 13:35:17 +0100

    dependecies update and db setup

--- backend/models.py
+++ backend/models.py
@@ -3,8 +3,10 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+database_user = 'student'
+database_password = 'student'
 database_name = 'trivia'
-database_path = 'postgres://{}/{}'.format('localhost:5432', database_name)
+database_path = 'postgresql://{}:{}@{}/{}'.format(database_user, database_password,'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
