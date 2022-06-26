6d840a6a6918467275d7b6ca9d51b8d84207f019
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2022-06-26 13:10:49 +0100

    fixed: dotenv, search questions

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -2,10 +2,9 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
-
 from flaskr import create_app
 from models import setup_db, Question, Category
-
+from settings import DB_TEST_NAME, DB_USER, DB_PASSWORD
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -14,8 +13,8 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgresql://student:student@{}/{}".format('localhost:5432', self.database_name)
+        self.database_name = DB_TEST_NAME
+        self.database_path = "postgresql://{}:{}@{}/{}".format(DB_USER, DB_PASSWORD,'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
--- frontend/src/components/QuestionView.js
+++ frontend/src/components/QuestionView.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React,{Component} from 'react';
 import '../stylesheets/App.css';
 import Question from './Question';
 import Search from './Search';
@@ -7,7 +7,7 @@ import $ from 'jquery';
 class QuestionView extends Component {
   constructor() {
     super();
-    this.state = {
+    this.state={
       questions: [],
       page: 1,
       totalQuestions: 0,
@@ -20,7 +20,7 @@ class QuestionView extends Component {
     this.getQuestions();
   }
 
-  getQuestions = () => {
+  getQuestions=() => {
     $.ajax({
       url: `/questions?page=${this.state.page}`, //TODO: update request URL
       type: 'GET',
@@ -41,17 +41,17 @@ class QuestionView extends Component {
   };
 
   selectPage(num) {
-    this.setState({ page: num }, () => this.getQuestions());
+    this.setState({page: num},() => this.getQuestions());
   }
 
   createPagination() {
-    let pageNumbers = [];
-    let maxPage = Math.ceil(this.state.totalQuestions / 10);
-    for (let i = 1; i <= maxPage; i++) {
+    let pageNumbers=[];
+    let maxPage=Math.ceil(this.state.totalQuestions/10);
+    for(let i=1;i<=maxPage;i++) {
       pageNumbers.push(
         <span
           key={i}
-          className={`page-num ${i === this.state.page ? 'active' : ''}`}
+          className={`page-num ${i===this.state.page? 'active':''}`}
           onClick={() => {
             this.selectPage(i);
           }}
@@ -63,7 +63,7 @@ class QuestionView extends Component {
     return pageNumbers;
   }
 
-  getByCategory = (id) => {
+  getByCategory=(id) => {
     $.ajax({
       url: `/categories/${id}/questions`, //TODO: update request URL
       type: 'GET',
@@ -82,13 +82,13 @@ class QuestionView extends Component {
     });
   };
 
-  submitSearch = (searchTerm) => {
+  submitSearch=(searchTerm) => {
     $.ajax({
-      url: `/questions`, //TODO: update request URL
+      url: `/questions/search`, //TODO: update request URL
       type: 'POST',
       dataType: 'json',
       contentType: 'application/json',
-      data: JSON.stringify({ searchTerm: searchTerm }),
+      data: JSON.stringify({searchTerm: searchTerm}),
       xhrFields: {
         withCredentials: true,
       },
@@ -108,9 +108,9 @@ class QuestionView extends Component {
     });
   };
 
-  questionAction = (id) => (action) => {
-    if (action === 'DELETE') {
-      if (window.confirm('are you sure you want to delete the question?')) {
+  questionAction=(id) => (action) => {
+    if(action==='DELETE') {
+      if(window.confirm('are you sure you want to delete the question?')) {
         $.ajax({
           url: `/questions/${id}`, //TODO: update request URL
           type: 'DELETE',
@@ -158,7 +158,7 @@ class QuestionView extends Component {
         </div>
         <div className='questions-list'>
           <h2>Questions</h2>
-          {this.state.questions.map((q, ind) => (
+          {this.state.questions.map((q,ind) => (
             <Question
               key={q.id}
               question={q.question}
